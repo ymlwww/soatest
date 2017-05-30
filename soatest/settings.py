264533930d4ccee95fc84dcbdcedc3742dc51563
。。。 <abc@deMacBook-Pro.local>
2017-05-30 22:41:15 +0800

    add index.html

--- soatest/settings.py
+++ soatest/settings.py
@@ -114,8 +114,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+ 
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "common_static"),
+    '/path/to/others/static/', 
+)
+ 
+
+
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
--- soatest/urls.py
+++ soatest/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from yml import views as yml_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', yml_views.index),
 ]
