5a330fe37d19a6d262e8cccf94fe6437eb0a1f44
。。。 <abc@deMacBook-Pro.local>
2017-06-06 14:35:25 +0800

    bug

--- yml/views.py
+++ yml/views.py
@@ -18,7 +18,7 @@ def item(request,a):
     c = int(a)
     print c
     name=["二校门","主楼","大礼堂","新清华学堂","工字厅","荷塘","综合体育馆","图书馆","西门","万人食堂"]
-    return render(request, 'item.html', {'result': c;'spotname': name[c-1]})
+    return render(request, 'item.html', {'result': c,'spotname': name[c-1]})
 
 def map(request):
     return render(request, 'map.html')    
