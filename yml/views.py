b0f4e233edeaf1043c2705cb7d31a338fd3638ea
。。。 <abc@deMacBook-Pro.local>
2017-06-06 14:57:01 +0800

    fix music2

--- yml/views.py
+++ yml/views.py
@@ -19,9 +19,9 @@ def item(request,a):
     print c
     name=["二校门","主楼","大礼堂","新清华学堂","工字厅","荷塘","综合体育馆","图书馆","西门","万人食堂"]
     print name[c-1]
-    musicurl = "http://http://13.65.151.139:8000/static/music/"+c+".mp3"
+    musicurl = "http://http://13.65.151.139:8000/static/music/"+a+".mp3"
     print musicurl
-    imgurl = "http://http://13.65.151.139:8000/static/img/"+c+".jpg"
+    imgurl = "http://http://13.65.151.139:8000/static/img/"+a+".jpg"
     print imgurl
     return render(request, 'item.html', {'result': c,'spotname': name[c-1],'musicurl':musicurl,'imgurl':imgurl})
 
